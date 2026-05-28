b5f20b627e86e887465f877e0dbfa282d4375056
Kendrick <kendrickoppong94@gmail.com>
2026-05-28 12:56:00 +0000

    refactor(web): remove obsolete layout components
    Remove AppShell, MapArea, and SidebarRight components as they are
    no longer required for the current sidebar implementation.

--- apps/web/src/components/layout/map-area.tsx
+++
@@ -1,5 +0,0 @@
-export const MapArea = () => (
-  <main className="relative col-start-2 row-start-2 overflow-hidden">
-    MapArea
-  </main>
-);
--- apps/web/src/components/layout/sidebar-right.tsx
+++
@@ -1,5 +0,0 @@
-export const SidebarRight = () => (
-  <aside className="col-start-3 row-start-2 flex flex-col overflow-hidden border-border border-l bg-background">
-    SidebarRight
-  </aside>
-);
